768dd11260487970c91798168a57b4783fb482f3
Yuliia M <myuliia2018@gmail.com>
2022-08-08 10:45:43 -0500

    add about me file

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,10 +5,13 @@ const AboutMe = () => {
     <div className='container py-5'>
         <div className='row'>
             <div className='col-lg-6 col-xm-12'>
-                <img src={author} alt='autor...'/>
+              <div className='photo-wrap mb-5'>
+
+                <img className='profile-img' src={author} alt='autor...'/>
+              </div>
             </div>
             <div className='col-lg-6 col-xm-12'>
-                <h1>About Me</h1>
+                <h1 className='about-heading' >About Me</h1>
                 <p>Hello! I am Yuliia. I recently earned a certificate in full stack development from the University of Texas bootcamp.
 I have been developing websites for over 6 months. I am Full-Stack Web Developer. Technologies I use is MERN (MongoDB, Express, ReactJs and NodeJs).
 </p>
